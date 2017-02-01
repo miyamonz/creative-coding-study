43de28dfad1679dee0c104bdd930de34cbcac971
Koichi Miyamoto <m5813k@gmail.com>
2017-02-01 18:14:31 +0900

    add src

---
+++ src/0.js
@@ -0,0 +1,10 @@
+let sketch = p => {
+    p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight);
+        p.background(0);
+    }
+    p.draw = () => {
+    }
+}
+
+export default sketch;
